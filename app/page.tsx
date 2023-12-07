1152d776bf787cb33c3085771ca5705b45462167
armeniocardoso <armeniotorres@gmail.com>
2023-12-07 18:13:51 -0300

    Autenticacao

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm">
         <h1>Estudos de Jamstack com Next.js</h1>
+        <Link href="/protegido">Área Protegida</Link>
       </div>
     </main>
   )
--- app/protegido/page.tsx
+++ app/protegido/page.tsx
@@ -1,11 +1,17 @@
-import Image from 'next/image'
+import { signOut } from '@/auth';
 
-export default function Home() {
+export default function Protegido() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Estudos de Jamstack com Next.js</h1>
-        <h2>Área Protegida</h2>
+        <h1>Área Protegida</h1>
+        <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
+          <form action={async () => { 'use server'; await signOut(); }} method="post">
+            <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Logout
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   )
